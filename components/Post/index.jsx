86ac319f07c2f0c10d9f6ddb1b84adea7219686c
KowalewskiPawel <pawelkowalewsk@gmail.com>
2021-12-20 22:44:38 +0100

    Posts component added

--- components/Post/index.jsx
+++ components/Post/index.jsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 
+import Wrapper from "./Wrapper";
+
 export default function Post({ post }) {
   return (
-    <div className='card'>
+    <Wrapper>
       <img src={post.frontmatter.cover_image} alt='' />
 
       <div className='post-date'>Posted on {post.frontmatter.date}</div>
@@ -12,8 +14,8 @@ export default function Post({ post }) {
       <p>{post.frontmatter.excerpt}</p>
 
       <Link href={`/blog/${post.slug}`}>
-        <a className='btn'>Read More</a>
+        <a>Read More</a>
       </Link>
-    </div>
+    </Wrapper>
   );
 }
---
+++ components/Posts/index.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Posts = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 80%;
+  height: auto;
+`;
+
+export default Posts;
