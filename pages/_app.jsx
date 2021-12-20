a8123970884164f76aea8be3dd5f0bf9fedebd30
KowalewskiPawel <pawelkowalewsk@gmail.com>
2021-12-20 18:57:11 +0100

    globalStyles added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,4 @@
 import Header from "../components/Header";
-import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
---
+++ styles/utils.js
@@ -0,0 +1,29 @@
+export const getColor =
+  (colorName) =>
+  ({ theme }) =>
+    theme.color[colorName];
+
+export const getFontFamily =
+  (fontName) =>
+  ({ theme }) =>
+    theme.fontFamily[fontName];
+
+export const getFontWeight =
+  (fontWeightName) =>
+  ({ theme }) =>
+    theme.fontWeight[fontWeightName];
+
+export const getMedias =
+  (mediasName) =>
+  ({ theme }) =>
+    theme.medias[mediasName];
+
+export const getAnimation =
+  (animationName) =>
+  ({ theme }) =>
+    theme.animations[animationName];
+
+export const getShadow =
+  (shadowName) =>
+  ({ theme }) =>
+    theme.shadows[shadowName];
