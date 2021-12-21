54233fc2f9c0cef6f2f47dcefd384d00fe21aa30
KowalewskiPawel <pawelkowalewsk@gmail.com>
2021-12-21 15:35:39 +0100

    added grid for posts

--- components/Post/Wrapper.jsx
+++ components/Post/Wrapper.jsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
+import { getShadow } from "../../styles/utils";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  padding: 15px;
+  border-radius: 10px;
+  box-shadow: ${getShadow("boxShadow")};
+`;
 
 export default Wrapper;
--- components/Posts/index.jsx
+++ components/Posts/index.jsx
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
+import { getMedias } from "../../styles/utils";
+
 const Posts = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 80%;
-  height: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 30px;
+  margin-top: 30px;
+
+  @media (max-width: ${getMedias("mobile")}) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export default Posts;
